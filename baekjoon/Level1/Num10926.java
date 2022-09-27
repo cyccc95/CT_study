a67f0ea3a98beedcb38ac144515b5680403cc8e8
cyccc95 <cyccc95@naver.com>
2022-09-27 21:20:30 +0900

    입출력을 응용하는 문제??!

---
+++ baekjoon/Level1/Num10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Num10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str + "??!");
+    }
+}
+
+// 입출력을 응용하는 문제??!
