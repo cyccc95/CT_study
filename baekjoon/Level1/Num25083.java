37e6f2a937d0cec2d010e2c96a05c0013dd19a12
cyccc95 <cyccc95@naver.com>
2022-09-27 21:21:41 +0900

    ',`,\ 등의 문자에 주의하며 새싹을 출력하는 문제

---
+++ baekjoon/Level1/Num25083.java
@@ -0,0 +1,12 @@
+public class Num25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
+
+// ', ", `, \ 등의 문자에 주의하며 새싹을 출력하는 문제
