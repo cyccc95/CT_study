a33f7b797382f4f3a4876fa96e2b94edd1ed9bbd
cyccc95 <cyccc95@naver.com>
2022-09-27 21:20:47 +0900

    곱셈 문제

---
+++ baekjoon/Level1/Num10998.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Num10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a*b);
+    }
+}
+
+// 곱셈 문제
