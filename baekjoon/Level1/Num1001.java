c3cb34682a0d82949753653c0a0ddc673e9a78a7
cyccc95 <cyccc95@naver.com>
2022-09-27 21:14:09 +0900

    두 수를 입력받고 뺄셈을 한 결과를 출력하는 문제

---
+++ baekjoon/Level1/Num1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Num1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a-b);
+    }
+}
+
+// 두 수를 입력받고 뺄셈을 한 결과를 출력하는 문제
