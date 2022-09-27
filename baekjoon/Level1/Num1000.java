3541261c30b4b5e780bb515ef58bff158d541c71
cyccc95 <cyccc95@naver.com>
2022-09-27 21:13:06 +0900

    두 수를 입력받고 합을 출력하는 문제

---
+++ baekjoon/Level1/Num1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Num1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
+
+// 두 수를 입력받고 합을 출력하는 문제
