c0e93efff15427f3613e0101205be57dfb56f4e8
cyccc95 <cyccc95@naver.com>
2022-09-27 21:21:07 +0900

    식을 직접 세워서 계산하는 문제

---
+++ baekjoon/Level1/Num18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Num18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year-543);
+    }
+}
+
+// 식을 직접 세워서 계산하는 문제
