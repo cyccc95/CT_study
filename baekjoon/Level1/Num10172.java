79c2671cc3c80f1823952069d2d81b0672d47766
cyccc95 <cyccc95@naver.com>
2022-09-27 21:18:40 +0900

    \,' 등의 문자에 주의하며 개를 출력하는 문제

---
+++ baekjoon/Level1/Num10172.java
@@ -0,0 +1,13 @@
+public class Num10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+                
+    }
+}
+
+// ", `, \ 등의 문자에 주의하며 개를 출력하는 문제
