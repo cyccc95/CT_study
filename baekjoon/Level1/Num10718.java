deab74575e9f5c1ab167366afdca66f671717076
cyccc95 <cyccc95@naver.com>
2022-09-27 21:19:45 +0900

    주어진 예제처럼 출력하는 문제

---
+++ baekjoon/Level1/Num10718.java
@@ -0,0 +1,8 @@
+public class Num10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
+
+// 주어진 예제처럼 출력하는 문제 2
