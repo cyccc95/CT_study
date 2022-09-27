e1ed1c3ce579ca5d63414ed71b13071aa2834825
cyccc95 <cyccc95@naver.com>
2022-09-27 21:17:48 +0900

    \,' 등의 문자에 주의하며 고양이를 출력하는 문제

---
+++ baekjoon/Level1/Num10171.java
@@ -0,0 +1,10 @@
+public class Num10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\ ");
+        System.out.println(" )  ( ') ");
+        System.out.println("(  /  ) ");
+        System.out.println(" \\(__)|");
+    }
+}
+
+// \, ' 등의 문자에 주의하며 고양이를 출력하는 문제
