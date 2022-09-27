01c9b722e9acb82c20d619423688092e5f3f279f
cyccc95 <cyccc95@naver.com>
2022-09-27 22:30:25 +0900

    두 수를 비교한 결과를 출력하는 문제

---
+++ baekjoon/Level2/Num1330.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Num1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b){
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
+
+// 두 수를 비교한 결과를 출력하는 문제
