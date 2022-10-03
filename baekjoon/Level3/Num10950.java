c1db9c96f6d547fcba73304e7d8cf2602b9ab57c
cyccc95 <cyccc95@naver.com>
2022-10-03 18:24:46 +0900

    A+B를 여러 번 출력하는 문제

---
+++ baekjoon/Level3/Num10950.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Num10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+
+        for (int i = 0; i < testCase; i++){
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            System.out.println(num1 + num2);
+        }
+    }
+}
+
+// A+B를 여러 번 출력하는 문제
