54af2b5e7c4adda981ec96a65a325cce6a082a78
cyccc95 <cyccc95@naver.com>
2022-10-06 23:27:38 +0900

    아스키 코드에 대해 알아보는 문제

---
+++ baekjoon/Level6/Num11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Num11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println((int)str.charAt(0));
+    }
+}
+
+// 아스키 코드에 대해 알아보는 문제
