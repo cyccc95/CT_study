70d9dc92429fd8b1e8e2f08cae895aa1d29adf33
cyccc95 <cyccc95@naver.com>
2022-11-10 20:59:20 +0900

    k번째로 큰 수를 구하는 문제

---
+++ baekjoon/Level10/Num25305.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Num25305 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[n - k]);
+    }
+}
+
+// k번째로 큰 수를 구하는 문제
