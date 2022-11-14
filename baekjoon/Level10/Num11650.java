cd657881611526a80c4e8c54c2a7beba92e4a52f
cyccc95 <cyccc95@naver.com>
2022-11-14 14:10:55 +0900

    좌표를 정렬하는 문제

---
+++ baekjoon/Level10/Num11650.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Num11650 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0];
+			}
+		});
+
+		for (int[] a : arr) {
+			bw.write(a[0] + " " + a[1] + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+
+}
+
+// 좌표를 정렬하는 문제
